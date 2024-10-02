578fc6019761a5d3a6d672ca35b1aa4a0da9d281
MimmoLB <147382057+LB-dev99@users.noreply.github.com>
2024-10-03 00:18:14 +0200

    Molti cambiamenti di identità

---
+++ src/components/energy/sections/services/data.ts
@@ -0,0 +1,58 @@
+import img1 from "./assets/solar3.png";
+import img2 from "./assets/wind.png";
+// TODO
+import img3 from "./assets/hydro.png";
+import img4 from "./assets/storage.png";
+// TODO
+import img5 from "./assets/smart-grid.png";
+import img6 from "./assets/green.png";
+
+export const servicesSectionData = {
+  title: "Alcune cose di cui mi occupo:",
+  subheading: {
+    text1_1: "",
+    text1_2: "",
+    text2: "",
+  },
+  services: [
+    {
+      title: "Progettazione e Pianificazione",
+      briefDescription:
+        "Dalla prima idea alla realizzazione del progetto. Offro servizi di progettazione architettonica, strutturale e impiantistica, garantendo soluzioni innovative e sostenibili. (Studi di fattibilità, Progettazione esecutiva CAD e BIM, calcoli strutturali)",
+      visual: img1,
+    },
+    {
+      title: "Direzione Lavori e Gestione Cantiere",
+      briefDescription:
+        "Seguo da vicino ogni fase del cantiere, assicurando la qualità dei lavori, il rispetto dei tempi e del budget. (Coordinamento della sicurezza, gestione delle forniture, controllo qualità, collaudi, contabilità lavori)",
+      visual: img2,
+    },
+    {
+      title: "Consulenza Tecnica",
+      briefDescription:
+        "Metto a disposizione la mia esperienza per risolvere problematiche tecniche complesse e fornire consulenze specializzate. (Perizie tecniche, diagnosi di patologie edilizie, diagnosi energetiche, supporto alle imprese, analisi dei rischi)",
+      visual: img3,
+    },
+    {
+      title: "Sicurezza e Ambiente",
+      briefDescription:
+        "Garantisco la massima sicurezza sul lavoro e il rispetto dell'ambiente. (Valutazione dei rischi, piani di sicurezza e coordinamento della sicurezza)",
+      visual: img4,
+    },
+    {
+      title: "Burocrazia e Certificazioni",
+      briefDescription:
+        "Fornisco certificazioni e ti accompagno nell'iter burocratico, semplificando le procedure e ottenendo le necessarie autorizzazioni. (APE, Enea, Allaccio Fotovoltaico, Pratiche edilizie, Certificazioni energetiche, collaudi statici, pratiche catastali)",
+      visual: img5,
+    },
+    {
+      title: "Innovazione e Sostenibilità",
+      briefDescription:
+        "Sono sempre alla ricerca di soluzioni innovative e sostenibili per rispondere alle esigenze di un mondo in continua evoluzione.(Progettazione di edifici a basso impatto ambientale, utilizzo di materiali eco-compatibili, implementazione di sistemi di domotica, progettazione di impianti fotovoltaici)",
+      visual: img6,
+    },
+  ],
+  callToAction:
+    "Discover the Future of Green Energy. Choose Your Sustainable Solution Today!",
+  buttonText: "Explore More",
+};
